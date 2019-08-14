d4ec5ade1fee35c39f9a5a606db30ffe797566c3
swagner <swagner@marchex.com>
2019-08-14 11:22:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+= Kubernetes Service / Pod / Ingress demo
+
+== Files
+
+- apple.yaml: defines a Service that pulls a simple echo service Docker image and configures it to return "apple".
+- banana.yaml: defines a Service that pulls a simple echo service Docker image and configures it to return "banana".
+- orange.yaml: defines a Service that pulls a simple echo service Docker image and configures it to return "orange".
+- ingress.yaml: defines two Ingress resource; one to route /apple to the apple Service; one to route /banana to the banana Service.
+- ingress-orange.yaml: defines a single Ingress resource that routes /orange to the orage Service.
+
+== Running
+
+x- Run `kubectl get service` ; shows a list of defined Services 
+x- Run `kubectl get pod` ; shows a list of defined Pods
+x- Run `kubectl get ingress` ; shows a list of defined Ingresses
+x- Run `kubectl apply -f apple.yaml` ; creates the Apple Pod and Service
+x- Run `kubectl apply -f banana.yaml` ; creates the Banana Pod and Service
+x- Run `kubectl get service` ; shows the two services have been created
+- Run `kubectl describe service apple` ; shows that the Service is of type 'ClusterIP' and not 'NodePort'
+- Run `kubectl apply -f ingress.yaml` ; creates the Ingress resource
+- Run `kubectl describe ingress example-ingress` ; shows the Ingress definition, grab the IP address
+- Run `curl -kL http://<ingress_ip>:8080/apple` ; should respond with "apple" (note: it may take a few seconds to create and propagate the nginx configuration)
+- Run `curl -kL http://<ingress_ip>:8080/banana` ; should respond with "banana"
+- Run `curl -kL http://<ingress_ip>:8080/orange` ; should return a 404
+- Run `kubectl apply -f orange.yaml` ; creates the orange Service
+- Run `curl -kL http://<ingress_ip>:8080/orange` ; still a 404
+- Run `kubectl apply ingress-orange.yaml` ; creates the orange-ingress
+- Run `curl -kL http://<ingress_ip>:8080/orange` ; should respond with "orange" after the spin-up time
+
+== Cleanup
+- Run `kubectl delete -f ingress-orange.yaml`
+- Run `kubectl delete -f orange.yaml`
+- Run `kubectl delete -f ingress.yaml`
+- Run `kubectl delete -f apple.yaml`
+
+
---
+++ run-demo.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+function main() {
+  clear
+
+  showMessage "Running k8s Ingress demo..."
+  askContinue
+
+  showMessage "Ensure Ingress Controller is enabled..."
+  minikube addons enable ingress
+  askContinue
+
+  showMessage "Here's a list of all the currently defined Services..."
+  listResources service
+  askContinue
+
+  showMessage "Here's a list of all the currently defined Pods..."
+  listResources pod
+  askContinue
+
+  showMessage "Here's a list of all the currently defined Ingresses..."
+  listResources ingress
+  askContinue
+
+  showMessage "Creating apple Service and Pod..."
+  createResource "apple.yaml"
+  askContinue
+
+  showMessage "Creating banana Service and Pod..."
+  createResource "banana.yaml"
+  askContinue
+
+  showMessage "Here's the list of all the currently defined Services..."
+  listResources service
+  askContinue
+
+  showMessage "Here are the details of the apple Service, notice it is of type ClusterIP, not NodePort..."
+  describeResource service "apple-service"
+  askContinue
+
+  showMessage "Creating the Ingress for the apple and banana Services..."
+  createResource "ingress.yaml"
+  askContinue
+
+  showMessage "Here are the details of the example-ingress Ingress..."
+  describeResource ingress "example-ingress"
+  askContinue
+
+  showMessage "We need the IP Address of the Ingress..."
+  lookUpIngressIp
+
+  echo "THE INGRESS IP IS: $ingress_ip"
+  askContinue
+
+  showMessage "Calling the apple Service..."
+  getFruitWithRetry apple
+  askContinue
+
+  showMessage "Calling the banana Service..."
+  getFruitWithRetry banana
+  askContinue
+
+  showMessage "Calling the orange Service..."
+  getFruitWithRetry orange
+  askContinue
+
+  showMessage "Creating the orange Service and Pod..."
+  createResource "orange.yaml"
+  askContinue
+
+  showMessage "Creating the Ingress for the orange Service..."
+  createResource "ingress-orange.yaml"
+  askContinue
+
+  showMessage "Calling the orange Service..."
+  getFruitWithRetry orange
+
+  askContinue
+
+  showMessage "This concludes the demonstration."
+
+  askContinue "continue (all resources will be deleted)?"
+
+
+  cleanUp
+}
+
+function askContinue() {
+  echo
+  message=${1-continue?}
+  pause $message
+  clear
+}
+
+#function getFruit() {
+#  curl -kL http://$ingress_ip/$1
+#}
+
+function getFruitWithRetry() {
+  FRUIT=$1
+  curlStatus=`curl -s -o /dev/null -w "%{http_code}" -kL http://$ingress_ip/$FRUIT` > /dev/null
+#  echo "  $FRUIT        $curlStatus"
+  if [ $curlStatus -eq 200 ] 
+  then
+    curl -kL http://$ingress_ip/$FRUIT
+  else
+    read -p "curl failed ($curlStatus), try again?  " yn
+    case $yn in
+      [Yy] ) getFruitWithRetry $FRUIT;; 
+      [Nn] ) ;;
+      * ) echo "Please respond with Y, y, N, or n...  "; getFruitWithRetry $FRUIT;;
+    esac
+  fi
+}
+
+function cleanUp() {
+  deleteResource "apple.yaml"
+  deleteResource "banana.yaml"
+  deleteResource "orange.yaml"
+  deleteResource "ingress.yaml"
+  deleteResource "ingress-orange.yaml"
+}
+
+function createResource() {
+  kubectl apply -f $1
+}
+
+function deleteResource() {
+  kubectl delete -f $1
+}
+
+function describeResource() {
+  kubectl describe $1 $2
+}
+
+function lookUpIngressIp() {
+  iip=""
+  while [ -z "$iip" ]
+  do
+    sleep 1s
+    echo "(looking up Ingress IP...)"
+    iip=$(kubectl describe ingress example-ingress | grep Address\: | tr -s ' ' | cut -d' ' -f2)
+  done
+  export ingress_ip=$iip
+}
+
+function listResources() {
+  resourceName=$1
+  kubectl get $resourceName
+}
+
+function pause() {
+  read -p "$*"
+}
+
+function showMessage() {
+  BLUE=`tput setaf 4`
+  GREEN=`tput setaf 2`
+  WHITE=`tput setaf 7`
+  BOLD=`tput smso`
+  NRML=`tput rmso`
+  echo -e  "${BOLD}${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${NRML}"
+  echo
+  echo -e "${WHITE}$*"
+  echo
+  echo -e  "${BOLD}${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${BLUE} - ${GREEN} - ${NRML}"
+  echo -e "${NRML}${WHITE}"
+}
+
+main
+
+#lookUpIngressIp
+#getFruitWithRetry orange
+
