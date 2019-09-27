8122450f36cd7e30eef22c043e610bd461b03483
swagner <swagner@marchex.com>
2019-09-27 09:12:57 -0700

    Namespacing things

--- cleanUp.sh
+++ cleanUp.sh
@@ -7,4 +7,6 @@ kubectl delete -f orange.yaml
 kubectl delete -f banana.yaml
 kubectl delete -f apple.yaml
 
-minikube addons disable ingress
+kubectl delete -f namespace-marchex.yaml
+
+##minikube addons disable ingress
